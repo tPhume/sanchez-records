af9cb9eaa6e94be3f51bc5e17e285fe227d7dc60
Traphume <traphumedev@gmail.com>
2020-06-16 18:25:04 +0700

    Added project color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clight: "#eeeeee",
+        cteal: "#00adb5",
+        cGray: "#393e46",
+        cDark: "#222831",
+      },
+    },
   },
   variants: {},
   plugins: [],
