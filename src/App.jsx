25845eb7e1fd8847b3a43e5d1214d264ba2364c1
Traphume <traphumedev@gmail.com>
2020-06-17 18:35:51 +0700

    Added svgr. COnfigured webpack

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ReactDom from "react-dom";
 import SideBar from "./components/SideBar/SideBar";
 import SideBarItem from "./components/SideBarItem/SideBarItem";
 
+import RickSanchez from "./Icons/RickSanchez.svg";
+
 import "./App.css";
 
 const CHARACTER = "Character";
@@ -16,6 +18,7 @@ function App() {
   return (
     <main className="flex h-screen w-screen bg-clight">
       <SideBar>
+        <RickSanchez width="40px" height="40px" className="mt-4 mb-2" />
         <SideBarItem name={CHARACTER} current={current} action={setCurrent} />
         <SideBarItem name={LOCATION} current={current} action={setCurrent} />
         <SideBarItem name={EPISODE} current={current} action={setCurrent} />
