9f1f7ee3d442135beca7d47d636b48037b3a19bc
Traphume <traphumedev@gmail.com>
2020-06-29 02:32:38 +0700

    Addeded to main. Removed image from LocationCard component

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -6,11 +6,6 @@ function LocationCard({ info, key }) {
       className="max-w-sm bg-white shadow-lg rounded-lg overflow-hidden m-4"
       key={key}
     >
-      <img
-        src={info.image}
-        alt="location"
-        className="w-full object-cover object-center"
-      />
       <div className="py-3 px-5">
         <h1 className="py-1 text-2xl font-semibold text-cteal">{info.name}</h1>
         <div className="card-info">
